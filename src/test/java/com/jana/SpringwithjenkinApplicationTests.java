8f866562b6bfa19319bf9fd531a89925ba985a25
jketham <jketham@gmail.com>
2023-10-04 13:18:17 +0530

    added

---
+++ src/test/java/com/jana/SpringwithjenkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwithjenkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
